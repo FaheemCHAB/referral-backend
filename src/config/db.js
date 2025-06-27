98a9a5afdb498e81ee601aecc3cf79fb9cc5c3c2
FaheemCHAB <mdfaheem.aitrich@gmail.com>
2025-06-27 14:15:01 +0530

    fix: added log to connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose")
 const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI)
-        console.log("Connected to DB");
+        console.log(`Connected to MongoDB: ${mongoose.connection.name}`);
+        console.log(`Environment: ${process.env.NODE_ENV}`);
     } catch (error) {
         console.log(error);
     }
